7441fa58b756c0be1ff75bb88a058c355a96c428
Kevin Gu <kevingu712@gmail.com>
2023-02-04 07:12:04 +0800

    Update benchlite.sh

--- benchlite.sh
+++ benchlite.sh
@@ -70,14 +70,14 @@ speed_test() {
 }
 
 speed() {
-    speed_test '1782'  'Comcast (Seattle, WA, US)                 '
     speed_test '2398'  'Studio Funk GmbH (Hamburg, Germany)       '
     speed_test '6151'  'Burstfire Networks (London, UK)           '
     speed_test '2023'  'LaFibre.info (Lyon, France)               '
     speed_test '3386'  'NFOrce Entmt (Amsterdam, Netherlands)     '
     speed_test '1267'  'Yes Optus (Sydney, Australia)             '
+    speed_test '1782'  'Comcast (Seattle, WA, US)                 '    
     speed_test '2054'  'Viewqwest Pte Ltd (Singapore)             '    
-    speed_test '5249'  'Ookla (Seoul, South Korea)                '    
+    speed_test '6842'  'Random Systems (Gimhae, South Korea)      '    
     speed_test '6405'  'Allied Telesis Capital (Misawa, Japan)    '
     speed_test '3633'  'China Telecom (Shanghai, China)           '
 }
